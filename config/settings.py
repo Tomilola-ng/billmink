9e9f658daaa9f8f37166d7dfeda9aa7a142dee77
unknown <tee.o2809@gmail.com>
2023-05-10 09:42:56 +0100

    Deploy commit

--- config/settings.py
+++ config/settings.py
@@ -108,3 +108,16 @@ MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 DOMAIN_PATH = 'localhost:8000'
+
+"""
+    Deployment Settings
+"""
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
